152fbdd95e1c26f18fb345ed18a377a7532f6680
Nitu Sherawat <120820816+Nitu-Sherawat@users.noreply.github.com>
2024-10-16 15:47:54 +0000

    dataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict" // use newer version of JS
+
+// alert(3+3) => we are using node not browser
+
+let age = 17;
+let name = "Nitu Sherawat";
+let isLoggedIn = true;
+let state = null;
+let count ;
+
+console.log(typeof age);
+console.log(typeof "Hello"); // name ?? name cut why why??
+console.log(typeof state);  // Null => object 
+console.log(typeof count); // Undefined
+console.log(typeof isLoggedIn);
+
+// Primitive dataTypes
+
+/*
+Number => range 2 power 53
+String 
+Boolean
+Undefined
+Bigint
+undefined 
+symbol => uniqueness
+*/
+
+// Object 
+
+/* 
+    Null => Standalone value
+*/
